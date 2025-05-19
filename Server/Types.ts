54dd848104fcb6b8c5cd395b5f1d3bd5aaa95b5b
IceBrick <icebrick0312@gmail.com>
2025-05-19 16:56:29 +0800

    Bump version to 1.8.4

--- Server/Types.ts
+++ Server/Types.ts
@@ -3,7 +3,7 @@ import type { Scope, Element } from '@lightbery/scope'
 type RenderScope = Scope<undefined>
 type ScriptScope = Scope<undefined>
 
-type Page = (scope: RenderScope, args: any) => {
+type Page = (scope: RenderScope, args?: any) => {
   title: string
   description: string
   keywords?: string
